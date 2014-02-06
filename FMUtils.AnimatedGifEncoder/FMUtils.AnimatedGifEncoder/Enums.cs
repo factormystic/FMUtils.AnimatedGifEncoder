618ab37743cd436646ef246aeed8f420aaaae854
factormystic <factormystic@gmail.com>
2014-02-05 22:46:50 -0500

    documentation, usings

--- FMUtils.AnimatedGifEncoder/FMUtils.AnimatedGifEncoder/Enums.cs
+++ FMUtils.AnimatedGifEncoder/FMUtils.AnimatedGifEncoder/Enums.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace FMUtils.AnimatedGifEncoder
 {
@@ -25,7 +22,18 @@ namespace FMUtils.AnimatedGifEncoder
     public enum FrameOptimization
     {
         None,
+
+        /// <summary>
+        /// Checks each frame to see if it's identical to the previous frame.
+        /// If so, it will extend the duration of the previous frame and skip writing the current frame.
+        /// If there are many duplicate frames, this optimization can greatly speed up processing since fewer quantizations can be run.
+        /// </summary>
         DiscardDuplicates,
+
+        /// <summary>
+        /// Using an automatically generated transparency color, erases pixels in the current frame which are identical to the composite image at that point.
+        /// If there are few changes between frames, this optimization can greatly reduce file size since LZW compresses this well.
+        /// </summary>
         AutoTransparency,
     };
 }
